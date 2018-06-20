62c0bd25436a4906d3a08aa4865651dc3429920d
francois branciard <francois.branciard@iex.ec>
2018-06-20 15:02:28 +0000

    fix mkdir optional

--- xmrig/entrypoint.sh
+++ xmrig/entrypoint.sh
@@ -3,7 +3,7 @@
 timeout=$1
 args=${@:${#timeout}}}
 echo "start xmrig with arg : ${args} and timeout: ${timeout}"
-mkdir iexec
+mkdir -p iexec
 (xmrig ${args} ) & pid=$!
 sleep ${timeout}  && echo "mined" >> iexec/consensus.iexec
 cat /iexec/consensus.iexec
