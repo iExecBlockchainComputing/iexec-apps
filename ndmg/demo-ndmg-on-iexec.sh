2ec0ee26a88b51484e1a8bc91d887d54f6a871ce
francois branciard <francois.branciard@iex.ec>
2018-06-21 12:35:08 +0000

    fix demo-ndmg-on-iexec.sh

--- ndmg/demo-ndmg-on-iexec.sh
+++ ndmg/demo-ndmg-on-iexec.sh
@@ -12,4 +12,7 @@ echo "generate /iexec/consensus.iexec"
 
 cat /tmp/ndmg_demo_dwi.log | grep Computing >> /iexec/consensus.iexec 
 cat /tmp/ndmg_demo_dwi.log | grep "Subject Means" >> /iexec/consensus.iexec 
-cat /iexec/consensus.iexec
+if [ -f /tmp/ndmg_demo/outputs/qa/graphs/desikan-res-4x4x4/desikan-res-4x4x4_plot.html ]
+then
+	cp -f /tmp/ndmg_demo/outputs/qa/graphs/desikan-res-4x4x4/desikan-res-4x4x4_plot.html /iexec/desikan-res-4x4x4_plot.html
+fi
