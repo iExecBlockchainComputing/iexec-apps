596bc574602b1780e2692fedd4d6fff5af3838a5
Ugo Plouviez <up@iex.ec>
2018-12-21 10:38:31 +0100

    Create 500M file

--- IntegrationTests/BigUpload/generateBigFile.sh
+++ IntegrationTests/BigUpload/generateBigFile.sh
@@ -8,6 +8,7 @@ mkdir /iexec
 rm -f $resultFile $consensusFile
 
 touch $resultFile
-fallocate -l 5G $resultFile
+# count = number of Mb of the file
+dd if=/dev/urandom of=$resultFile bs=1048576 count=500
 
 echo "Determinist">> $consensusFile
