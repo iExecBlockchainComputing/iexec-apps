4618bd4418018be165b6473314576174173d6911
Hadrien Croubois <hc@iex.ec>
2019-05-31 15:48:11 +0200

    reducing the wait, the requester will have to ask a date in the past

--- PriceFeed/entrypoint.sh
+++ PriceFeed/entrypoint.sh
@@ -5,7 +5,10 @@ then
 	export APIKEY=`cat iexec_in/$DATASET_FILENAME`;
 fi
 
-export WAIT_MIN=1800000; # 30min
-export WAIT_MAX=2100000; # 35min
+export WAIT_MIN=0;      # 0min
+export WAIT_MAX=180000; # 3min
+
+#export WAIT_MIN=1800000; # 30min
+#export WAIT_MAX=2100000; # 35min
 
 node src/oracle.js $@
