570c0af585c9b2e22b2c7d498ff0599bdc32ed16
Hadrien Croubois <hc@iex.ec>
2019-06-03 18:15:32 +0200

    better determinism for webnotary

--- PriceFeed/entrypoint.sh
+++ PriceFeed/entrypoint.sh
@@ -5,8 +5,8 @@ then
 	export APIKEY=`cat iexec_in/$DATASET_FILENAME`;
 fi
 
-export WAIT_MIN=0;      # 0min
-export WAIT_MAX=180000; # 3min
+#export WAIT_MIN=0;      # 0min
+#export WAIT_MAX=180000; # 3min
 
 #export WAIT_MIN=1800000; # 30min
 #export WAIT_MAX=2100000; # 35min
