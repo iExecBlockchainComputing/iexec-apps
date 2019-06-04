ed2d00e4f2024804b3fba520932b4084f1b3d6bd
Ugo Plouviez <up@iex.ec>
2019-06-04 16:14:49 +0200

    Add randomness to non deterministic dapp

--- IntegrationTests/NonDeterminist/generateNonDeterminist.sh
+++ IntegrationTests/NonDeterminist/generateNonDeterminist.sh
@@ -8,9 +8,9 @@ mkdir /iexec_out
 rm -f $resultFile $consensusFile
 
 touch $resultFile
-echo $((1 + RANDOM % 10000)) >> $resultFile
+echo $((1 + RANDOM)) >> $resultFile
 
 touch $consensusFile
-echo $((1 + RANDOM % 10000)) >> $consensusFile
+echo $((1 + RANDOM)) >> $consensusFile
 
 echo "done"
