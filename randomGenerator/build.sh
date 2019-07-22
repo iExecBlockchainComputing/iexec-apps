0625d63333a19cc3d54e0201e8e0ae1469b8561d
Hadrien Croubois <hc@iex.ec>
2019-07-22 11:14:17 +0200

    Adding scone version of randomgenerator

--- randomGenerator/build.sh
+++ randomGenerator/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 docker image build -f Dockerfile-unsafe -t iexechub/random-generator-unsafe:0.0.1 .
+docker image build -f Dockerfile-scone  -t iexechub/random-generator-scone:0.0.1 .
