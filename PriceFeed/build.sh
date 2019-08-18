833b933217aed4f8c5eb51d941a61ea1ad7889d7
Hadrien Croubois <hc@iex.ec>
2019-08-18 13:17:05 +0200

    python version of the pricefeed (for TEE)

--- PriceFeed/build.sh
+++ PriceFeed/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker image build -f Dockerfile -t iexechub/app-price-oracle:0.0.3 .
+#docker image build -f Dockerfile-js        -t iexechub/app-price-oracle:0.0.3     .
+docker image build -f Dockerfile-js        -t iexechub/price-feed-js:0.0.3        .
+docker image build -f Dockerfile-py-unsafe -t iexechub/price-feed-py-unsafe:0.0.1 .
+docker image build -f Dockerfile-py-scone  -t iexechub/price-feed-py-scone:0.0.1  .
+
