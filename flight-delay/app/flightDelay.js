04ff0bb799e1a4e283bf41700c8f919c1477d3a8
Nikhil <nikhilyadav3000@gmail.com>
2019-12-03 17:46:07 +0530

    changed the api

--- flight-delay/app/flightDelay.js
+++ flight-delay/app/flightDelay.js
@@ -13,18 +13,22 @@ const errorFilePath       = `${root}/error.iexec`;
  *                                 ARGUMENTS                                 *
  *****************************************************************************/
 
-var [id , timestamp] = process.argv.slice(2).map(s => s);
+var [id, timestamp] = process.argv.slice(2).map(s => s);
+
+var appId = '0ddf43fd';
+var appKey = 'c8663643d1b8d907717810f38f97ffb1';
 
 // var timestampInMili = new Date().getTime();
 // var timestamp = parseInt(timestampInMili/1000)
 // console.log(longi);
 // console.log(lati);
 // console.log(timestamp);
+//flightID = 1023084433
 /*****************************************************************************
  *                                HTTP QUERY                                 *
  *****************************************************************************/
 
-let url = `https://iexec-flight-data.herokuapp.com/delay/${id},${timestamp}`;
+let url = `https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/${id}?appId=${appId}&appKey=${appKey}`;
 /*****************************************************************************
  *                                  EXECUTE                                  *
  *****************************************************************************/
@@ -32,19 +36,12 @@ axios.get(url)
 .then(res=>{
 	// console.log(res);
     var result = res.data;
-    if(result.status == "found")
-    {
-        var delay = parseInt(result.delay);
-        console.log(delay);
-        var iexeccallback = ethers.utils.defaultAbiCoder.encode(['uint256', 'uint256', 'uint256'], [timestamp, id, delay]);
-        var iexecconsensus = ethers.utils.keccak256(iexeccallback);
-        fs.writeFile(callbackFilePath,    iexeccallback , (err) => {});
-        fs.writeFile(determinismFilePath, iexecconsensus, (err) => {});
-    }
-    else
-    {
-        throw 'ivalid id';
-    }
+    var delay = parseInt(result.flightStatus.delays.departureGateDelayMinutes);
+    console.log(delay);
+    var iexeccallback = ethers.utils.defaultAbiCoder.encode(['uint256', 'uint256', 'uint256'], [timestamp, id, delay]);
+    var iexecconsensus = ethers.utils.keccak256(iexeccallback);
+    fs.writeFile(callbackFilePath,    iexeccallback , (err) => {});
+    fs.writeFile(determinismFilePath, iexecconsensus, (err) => {});
 	
 })
 .catch(err=>{
