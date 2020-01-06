600ca64e2f00e97ce62b230701feb6407f0f2937
Hadrien Croubois <hc@iex.ec>
2020-01-06 14:12:01 +0100

    trying to fix kaiko's pricefeed

--- PriceFeed/build.sh
+++ PriceFeed/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker image build -f Dockerfile-js        -t iexechub/app-price-oracle:0.0.3     .
-docker image build -f Dockerfile-js        -t iexechub/price-feed-js:0.0.3        .
-docker image build -f Dockerfile-py-unsafe -t iexechub/price-feed-py-unsafe:0.0.1 .
-docker image build -f Dockerfile-py-scone  -t iexechub/price-feed-py-scone:0.0.1  .
+docker image build -f Dockerfile-js        -t iexechub/app-price-oracle:0.0.3    .
+docker image build -f Dockerfile-js        -t iexechub/pricefeed-js:0.0.3        .
+docker image build -f Dockerfile-py-unsafe -t iexechub/pricefeed-py-unsafe:0.0.1 .
+docker image build -f Dockerfile-py-scone  -t iexechub/pricefeed-py-scone:0.0.1  .
