915e7923fb1fe70de3b5fb86c362ce68d790c2be
Hadrien Croubois <hc@iex.ec>
2020-02-04 13:39:28 +0100

    Update signer to support determinism without callback

--- PriceFeed-Kaiko/signer/signer.py
+++ PriceFeed-Kaiko/signer/signer.py
@@ -2,6 +2,7 @@
 
 import json
 import os
+import pathlib
 import traceback
 from web3.auto import w3
 from eth_account.messages import defunct_hash_message
@@ -9,9 +10,9 @@ from eth_account.messages import defunct_hash_message
 keccak256 = w3.soliditySha3
 
 # FOR DEBUGING
-# os.environ['enclave_key'] = "0x912cf4c9298141d745320abdb656c27d58778a5b2d4290186c0185ed9acda6d6"
-# os.environ['taskid']      = "0x171d4a18b30912aaef6c0baa08027607b9359fe1afbe4b4b158e829acd29aa12"
-# os.environ['worker']      = "0x1cb25226FeCeE496f246DDd1D735276B2E168B5a"
+# os.environ['enclave_key'] = '0x912cf4c9298141d745320abdb656c27d58778a5b2d4290186c0185ed9acda6d6'
+# os.environ['taskid']      = '0x171d4a18b30912aaef6c0baa08027607b9359fe1afbe4b4b158e829acd29aa12'
+# os.environ['worker']      = '0x1cb25226FeCeE496f246DDd1D735276B2E168B5a'
 
 root            = '/'
 sconeDir        = '{}scone/'.format(root)
@@ -20,46 +21,63 @@ callbackFile    = 'callback.iexec'
 determinismFile = 'determinism.iexec'
 enclaveSigFile  = 'enclaveSig.iexec'
 
+
+
+def isFile(path):
+	with pathlib.Path(path) as f:
+		return f.exists() and f.is_file()
+
 class Signer:
 	def __init__(self, key):
 		self.pk = key;
 
-	def signContribution(self, worker, taskid, resultDigest):
-		resultHash = keccak256([            "bytes32", "bytes32" ], [         taskid, resultDigest ])
-		resultSeal = keccak256([ "address", "bytes32", "bytes32" ], [ worker, taskid, resultDigest ])
-		signature = w3.eth.account.signHash(
-			defunct_hash_message(keccak256([ "bytes32", "bytes32" ], [ resultHash, resultSeal ])),
-			private_key=self.pk
-		).signature
+	def signContribution(self, worker, taskid, digest):
+		hash      = keccak256([            'bytes32', 'bytes32' ], [         taskid, digest ])
+		seal      = keccak256([ 'address', 'bytes32', 'bytes32' ], [ worker, taskid, digest ])
+		message   = keccak256([            'bytes32', 'bytes32' ], [         hash,   seal   ])
+		signature = w3.eth.account.signHash(defunct_hash_message(message), private_key=self.pk).signature
 
 		return {
-			'resultDigest': resultDigest.hex(),
-			'resultHash':   resultHash.hex(),
-			'resultSalt':   resultSeal.hex(), # TODO: rename and notify core
+			'resultDigest': digest,
+			'resultHash':   hash.hex(),
+			'resultSalt':   seal.hex(), # TODO: rename and notify core
 			'signature':    signature.hex(),
 		}
+
+
 
 if __name__ == '__main__':
 
 	try:
+		if isFile('{path}{file}'.format(path=sconeDir, file=callbackFile)):
+			with open('{path}{file}'.format(path=sconeDir, file=callbackFile), 'r') as file:
+				callback = file.read()
+				digest   = keccak256([ 'bytes' ], [ callback ]).hex()
 
-		with open('{path}{file}'.format(path=sconeDir, file=callbackFile), 'r') as file:
-			callback = file.read()
+		elif isFile('{path}{file}'.format(path=sconeDir, file=determinismFile)):
+			with open('{path}{file}'.format(path=sconeDir, file=determinismFile), 'r') as file:
+				callback = None
+				digest   = file.read()
+
+		else:
+			raise Exception('ERROR: no callback and no determinism available')
 
 		data = Signer(os.environ['enclave_key']).signContribution(
-			worker       = os.environ['worker'],
-			taskid       = os.environ['taskid'],
-			resultDigest = keccak256([ "bytes"], [ callback ])
+			worker = os.environ['worker'],
+			taskid = os.environ['taskid'],
+			digest = digest
 		)
 
-		with open('{path}{file}'.format(path=outputDir, file=callbackFile), 'w') as file:
-			file.write(callback)
+		if callback:
+			with open('{path}{file}'.format(path=outputDir, file=callbackFile), 'w') as file:
+				file.write(callback)
 
 		with open('{path}{file}'.format(path=outputDir, file=determinismFile), 'w') as file:
-			file.write(data['resultDigest'])
+			file.write(digest)
 
 		with open('{path}{file}'.format(path=outputDir, file=enclaveSigFile), 'w') as file:
 			file.write(json.dumps(data))
+
 
 	except Exception as ex:
 		traceback.print_exc()
