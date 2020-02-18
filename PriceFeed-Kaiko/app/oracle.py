15a9f2c457a5dc7f4804bfbbad65df3bb3e5347d
Pierre Jeanjacquot <pierre.jeanjacquot@gadz.org>
2020-02-18 13:19:57 +0100

    update business logic

--- PriceFeed-Kaiko/app/oracle.py
+++ PriceFeed-Kaiko/app/oracle.py
@@ -40,6 +40,11 @@ class Lib:
 			raise Exception('Missing API key dataset')
 
 	def fetchMarketData(region, endpoint, params):
+		print('Request https://{region}.market-api.kaiko.io/v1/data/trades.v1/{endpoint}?{params}'.format(
+			region   = region,
+			endpoint = endpoint,
+			params   = params,
+		))
 		return json.loads(
 			urllib.request.urlopen(
 				urllib.request.Request(
@@ -57,76 +62,46 @@ class Lib:
 		)
 
 class PriceFeed:
-	def fetchRate(baseAsset, quoteAsset, endpoint, interval, power, startTime=None):
-		if startTime:
-			return Lib.fetchMarketData(
-				'us',
-				'{endpoint}/{baseAsset}/{quoteAsset}'.format(baseAsset=baseAsset, quoteAsset=quoteAsset, endpoint=endpoint),
-				Lib.formatArgs({
-					'start_time': startTime.isoformat()[0:23]+'Z',
-					'interval':   interval,
-					'page_size':  1,
-				})
-			)
-		else:
-			return Lib.fetchMarketData(
-				'us',
-				'{endpoint}/{baseAsset}/{quoteAsset}/recent'.format(baseAsset=baseAsset, quoteAsset=quoteAsset, endpoint=endpoint),
-				Lib.formatArgs({
-					'interval': interval,
-					'limit':    1
-				})
-			)
+	def fetchRate(baseAsset, quoteAsset):
+		return Lib.fetchMarketData(
+			'us',
+			'spot_direct_exchange_rate/{baseAsset}/{quoteAsset}/recent'.format(baseAsset=baseAsset, quoteAsset=quoteAsset),
+			Lib.formatArgs({
+				'interval': '1m',
+				'limit':  	720,
+			})
+		)
 
-	def run(baseAsset, quoteAsset, endpoint, interval, power, startTime=None):
-		if startTime:
-			try:    startTime = datetime.datetime.utcfromtimestamp(int(startTime))
-			except: startTime = datetime.datetime.fromisoformat(startTime)
-
+	def run(baseAsset, quoteAsset, power):
 		response = PriceFeed.fetchRate(
 			baseAsset  = baseAsset,
 			quoteAsset = quoteAsset,
-			endpoint   = endpoint,
-			interval   = interval,
-			power      = power,
-			startTime  = startTime,
 		)
-		data      = response.get('data')[0]
-		timestamp = data.get('timestamp')
-		details   = '-'.join([ baseAsset, quoteAsset, power ])
-		rawValue  = data.get('price')
-		value     = Lib.parseValue(rawValue, 'uint256', power)
-		return (timestamp, details, value)
-
-class MarketData:
-	def run(region, endpoint, params, valueType, valuePath, power):
-		response = Lib.fetchMarketData(
-			region=region,
-			endpoint=endpoint,
-			params=params,
-		)
-		data      = response.get('data')[0]
-		timestamp = data.get('timestamp')
-		details   = '-'.join([ region, endpoint, params, valuePath, valueType, power ])
-		rawValue  = next(match.value for match in jsonpath_rw.parse(valuePath).find(data))
-		value     = Lib.parseValue(rawValue, valueType, power)
-		return (timestamp, details, value)
-
+		try:
+			data      = response.get('data')[0]
+			timestamp = data.get('timestamp')
+			details   = '-'.join([ baseAsset, quoteAsset, power ])
+			rawValue  = data.get('price')
+			value     = Lib.parseValue(rawValue, 'uint256', power)
+			return (timestamp, details, value)
+		except Exception as e:
+			raise Exception('API response parsing failure: {}'.format(e))
 
 
 class Entrypoints:
-	pricefeed  = PriceFeed.run
-	marketdata = MarketData.run
-
-
+	pricefeed = PriceFeed.run
 
 # Example usage:
-# pricefeed btc usd spot_direct_exchange_rate 1d 9 2019-12-01T12:00:00
+# pricefeed btc usd 9
 if __name__ == '__main__':
 	print("PriceFeed started")
 	try:
 		# EXECUTE CALL
-		(timestamp, details, value) = getattr(Entrypoints, sys.argv[1])(*sys.argv[2:])
+		(timestamp, details, value) = PriceFeed.run(
+			baseAsset  = sys.argv[2],
+			quoteAsset = sys.argv[3],
+			power      = sys.argv[4],
+		)
 		print('- Success: {} {} {}'.format(timestamp, details, value))
 
 		# GENERATE CALLBACK
