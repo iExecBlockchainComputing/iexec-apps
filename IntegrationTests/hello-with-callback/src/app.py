b39ce41bb3990d95e47a71b5f75fff0fc42544a7
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2020-05-04 15:58:03 +0200

    hello-with-callback

--- IntegrationTests/hello-with-callback/src/app.py
+++ IntegrationTests/hello-with-callback/src/app.py
@@ -1,4 +1,6 @@
 import os
+import sys
+from web3.auto import w3
 
 print("Started compute hello-with-callback")
 
@@ -6,10 +8,17 @@ dir = os.path.join("/iexec_out")
 if not os.path.exists(dir):
     os.mkdir(dir)
 
+hexChar = "a"
+
+if len(sys.argv) > 1:
+    hexChar = sys.argv[1]
+
 with open("iexec_out/callback.iexec", "w+") as fout:
-   resultTxt = "0x0000000000000000000000000000000000000000000000000000000000abcdef0000000000000000000000000000000000000000000000000000000000abcdef"
-   print(resultTxt)
-   fout.write(resultTxt)
+   callback = '0x000000000000000000000000000000000000000000000000000000000000000{}'.format(hexChar)
+   fout.write(callback)
+   print('Callback: {} (written to callback.iexec)'.format(callback))
+   digest = keccak256([ 'bytes' ], [ callback ]).hex()
+   print('Digest: {} (not written, but should match reveal `resultDigest`)'.format(digest))
 
 # touch 'completed-compute.iexec' file at end of compute
 open('/iexec_out/completed-compute.iexec', 'a').close()
