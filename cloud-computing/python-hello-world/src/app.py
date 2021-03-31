218576d8e979e3fd9b54d29d634be557e0ddc91a
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2021-03-31 11:28:44 +0200

    Python HelloWorld standard app can handle dataset with pretty filename

--- cloud-computing/python-hello-world/src/app.py
+++ cloud-computing/python-hello-world/src/app.py
@@ -5,6 +5,7 @@ from pyfiglet import Figlet
 
 iexec_out = os.environ['IEXEC_OUT']
 iexec_in = os.environ['IEXEC_IN']
+dataset_file = iexec_in + '/' + os.environ['IEXEC_DATASET_FILENAME']
 
 # Do whatever you want
 text = "Hello, World!"
@@ -12,10 +13,10 @@ if len(sys.argv) > 1:
     text = 'Hello, {}!'.format(sys.argv[1])
 text = Figlet().renderText(text) + text # Let's add some art for e.g.
 
-# Eventually use some confidential assets
-if os.path.exists(iexec_in + '/confidential-asset.txt'):
-    with open(iexec_in + '/confidential-asset.txt', 'r') as f:
-        text = text + '\nConfidential asset: ' + f.read()
+# Eventually use a dataset (public with Standard mode and confidential with TEE mode)
+if os.path.exists(dataset_file):
+    with open(dataset_file, 'r') as f:
+        text = text + '\nDataset ({}): {}'.format(dataset_file, f.read())
 
 # Append some results
 with open(iexec_out + '/result.txt', 'w+') as f:
@@ -25,10 +26,3 @@ with open(iexec_out + '/result.txt', 'w+') as f:
 # Declare everything is computed
 with open(iexec_out + '/computed.json', 'w+') as f:
     json.dump({ "deterministic-output-path" : iexec_out + '/result.txt' }, f)
-
-## Try:
-# Basic:
-# mkdir -p /tmp/iexec_out && IEXEC_OUT=/tmp/iexec_out IEXEC_IN=/tmp/iexec_in python3 app.py Alice
-#
-# Tee:
-# mkdir -p /tmp/iexec_out && IEXEC_OUT=/tmp/iexec_out IEXEC_IN=../tee/confidential-assets python3 app.py Alice
