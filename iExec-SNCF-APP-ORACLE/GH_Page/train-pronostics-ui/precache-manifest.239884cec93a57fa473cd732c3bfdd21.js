92aa0ccc21de6726a9a9498bdb0ce85f8b7a4936
sunydev <82972608+sunydev@users.noreply.github.com>
2021-12-23 12:12:56 -0400

    Add files via upload

---
+++ iExec-SNCF-APP-ORACLE/GH_Page/train-pronostics-ui/precache-manifest.239884cec93a57fa473cd732c3bfdd21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf876ae68b6f2247eaaa0436ab54136b",
+    "url": "/train-pronostics-ui/index.html"
+  },
+  {
+    "revision": "3ad38721bd0219dadaad",
+    "url": "/train-pronostics-ui/static/css/2.cd26dbc7.chunk.css"
+  },
+  {
+    "revision": "e5b8b4517054ae197766",
+    "url": "/train-pronostics-ui/static/css/main.060aeaaf.chunk.css"
+  },
+  {
+    "revision": "3ad38721bd0219dadaad",
+    "url": "/train-pronostics-ui/static/js/2.4b3225bc.chunk.js"
+  },
+  {
+    "revision": "7ee0693e48d7810c5ff155e5484601ac",
+    "url": "/train-pronostics-ui/static/js/2.4b3225bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b8b4517054ae197766",
+    "url": "/train-pronostics-ui/static/js/main.fd1b5633.chunk.js"
+  },
+  {
+    "revision": "44bc034d111d3e0156e7",
+    "url": "/train-pronostics-ui/static/js/runtime-main.3062e5ad.js"
+  }
+]);
